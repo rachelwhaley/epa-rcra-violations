ddf1e383d4439e9146f1477961266e489de63505
Esther Spurlock <eespurlock@cs.uchicago.edu>
2019-05-07 17:12:48 -0500

    wrote code for all models

--- Esther_Edith/models.py
+++ Esther_Edith/models.py
@@ -10,10 +10,12 @@ import numpy as np
 import sklearn.tree as tree
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, HuberRegressor,\
+    BayesianRidge
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier,\
-    GradientBoostingClassifier, AdaBoostClassifier, BaggingClassifier
+    GradientBoostingClassifier, AdaBoostClassifier, BaggingClassifier,\
+    IsolationForest, RandomTreesEmbedding
 
 #Defined constants for this assignment
 #models we will use
@@ -25,126 +27,93 @@ FOREST = "Random Forests"
 EXTRA = "Extra Trees"
 ADA_BOOSTING = "Ada Boosting"
 BAGGING = "Bagging"
+HUBER = "Huber Regressor"
+BAY = "Bayesian Ridge"
+ISOLATION = "Isolation Forest"
+EMBED = "Random Trees Embedding"
 
-def training_models(train_variable, train_features, test_variable,\
-    test_features):
+C_VALS = [0.5, 1.0, 1.5, 2.0, 2.5, 3.0]
+NEIGHBORS = [1, 2, 5, 10, 20, 50, 100, 200]
+MAX_DEPTH = [1, 5, 10, 20, 50, 100, 200, 300, 500]
+NUM_TREES = [1000, 5000, 10000, 15000, 20000]
+MAX_FEATURES = [0.1, 0.2, 0.3, 0.5, 0.7, 0.9]
+MAX_LEAVES = [10, 20, 50, 100, 200, 300, 500, 1000, None]
+N_ESTIMATORS = [10, 30, 50, 100, 150, 200, 300]
+LEARNING_RATE = [0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 5.0]
+MAX_SAMPLES = MAX_FEATURES[:]
+EPSILON = [1.1, 1.2, 1.3, 1.5, 1.7, 1.9]
+N_ITER = [50, 100, 300, 500, 700, 1000]
+
+def training_models():
     '''
-    Trains all models on training data
-
-    Inputs:
-        train_variable: pandas series of variable column for training set
-        train_features: pandas dataframe of features for training set
-        test_variable: pandas series of variable column for testing set
-        test_features: pandas dataframe of features for testing set
-
-    Outputs:
-        models_dict: a dictionary with all information about all models
     '''
-    models_dict = {}
+    models_lst = []
     
     #Set the value for all model types
-    models_dict[REGRESSION], models_dict[SVM] =\
-        regression_svm_modeling(train_variable, train_features, test_variable,\
-        test_features)
-    models_dict[KNN] = knn_modeling(train_variable, train_features,\
-        test_variable, test_features)
-    models_dict[FOREST], models_dict[EXTRA], models_dict[TREE] =\
-        forest_modeling(train_variable, train_features, test_variable,\
-        test_features)
-    models_dict[ADA_BOOSTING] = ada_boost_modeling(train_variable,\
-        train_features, test_variable, test_features)
-    models_dict[BAGGING] = bagging_modeling(train_variable, train_features,\
-        test_variable, test_features)
+    models_lst += regression_svm_modeling()
+    models_lst += knn_modeling()
+    models_lst += forest_modeling()
+    models_lst += ada_boost_modeling()
+    models_lst += bagging_modeling()
+    models_lst += huber_modeling()
+    models_lst += bays_modeling()
 
-    return models_dict
+    return models_lst
 
-def regression_svm_modeling(train_variable, train_features, test_variable,\
-    test_features):
+def bays_modeling():
+    '''
+    '''
+    models_lst = []
+    for n in N_ITER:
+        param = "Max Number of Iterations: " + str(n)
+        model = BayesianRidge(n_iter=n)
+        models_lst.append(tuple([BAY, param, model]))
+    return models_lst
+
+def huber_modeling():
+    '''
+    '''
+    models_lst = []
+    for e in EPSILON:
+        param = "Epsilon: " + str(e)
+        models_lst.append(tuple([HUBER, param, HuberRegressor(epsilon=e)]))
+    return models_lst
+
+def regression_svm_modeling():
     '''
     Creates multiple regression models
-
-    Inputs:
-        train_variable: pandas series of variable column for training set
-        train_features: pandas dataframe of features for training set
-        test_variable: pandas series of variable column for testing set
-        test_features: pandas dataframe of features for testing set
-
-    Outputs:
-        reg_dict: a dictionary with all information about all regression models
-        svm_dict: a dictionary with all information about all svm models
     '''
-    reg_dict = {}
-    svm_dict = {}
-    C_VALS = [0.5, 1.0, 1.5, 2.0, 2.5, 3.0]
+    models_lst = []
     for c in C_VALS:
         param = "C value: " + str(c)
-        model_unfit = LogisticRegression(C=c, solver='sag')
-        reg_dict[param] = test_models(model_unfit, False, train_variable,\
-            train_features, test_variable, test_features)
-        model_unfit = LinearSVC(C=c)
-        svm_dict[param] = test_models(model_unfit, True, train_variable,\
-            train_features, test_variable, test_features)
-    return reg_dict, svm_dict
+        model_SVM = LogisticRegression(C=c, solver='sag')
+        model_linear = LinearSVC(C=c)
+        models_lst.append(tuple([SVM, param, model_SVM]))
+        models_lst.append(tuple([REGRESSION, param, model_linear]))
+    return models_lst
 
 def knn_modeling(train_variable, train_features, test_variable,\
     test_features):
     '''
     Creates multiple nearest neighbors models
-
-    Inputs:
-        train_variable: pandas series of variable column for training set
-        train_features: pandas dataframe of features for training set
-        test_variable: pandas series of variable column for testing set
-        test_features: pandas dataframe of features for testing set
-
-    Outputs:
-        knn_dict: a dictionary with all information about all nearest neighbors
-            models
     '''
-    knn_dict = {}
-    NEIGHBORS = [1, 2, 5, 10, 20, 50, 100, 200]
+    models_lst = []
     for k in NEIGHBORS:
         param = "K Neighbors: " + str(k)
-        model_unfit = KNeighborsClassifier(n_neighbors=k)
-        knn_dict[param] = test_models(model_unfit, False, train_variable,\
-            train_features, test_variable, test_features)
-    return knn_dict
+        models_lst.append(tuple([KNN, param,
+            KNeighborsClassifier(n_neighbors=k)]))
+    return models_lst
 
-def forest_modeling(train_variable, train_features, test_variable,\
-    test_features):
+def forest_modeling():
     '''
     Creates multiple decision tree models, random forest models and extra trees
         models
-    (Random forests and extra trees take the same parameters, which is why
-    we are putting them together. We will use the same depth for decision
-    trees which is why this is with them)
-
-    Inputs:
-        train_variable: pandas series of variable column for training set
-        train_features: pandas dataframe of features for training set
-        test_variable: pandas series of variable column for testing set
-        test_features: pandas dataframe of features for testing set
-
-    Outputs:
-        forest_dict: a dictionary with all information about all random forest
-            models
-        extra_dict: a dictionary with all information about all extra tree
-            models
-        tree_dict: a dictionary with all information about all decision tree
-            models
     '''
-    forest_dict = {}
-    extra_dict = {}
-    tree_dict = {}
-    MAX_DEPTH = [1, 5, 10, 20, 50, 100, 200, 300, 500]
-    NUM_TREES = [1000, 5000, 10000, 15000, 20000]
-    MAX_FEATURES = [0.01, 0.1, 0.2, 0.5, 0.7, 0.9]
-    MAX_LEAVES = [10, 20, 50, 100, 200, 300, 500, 1000, None]
+    models_lst = []
     for depth in MAX_DEPTH:
         tree_param = "Max Depth of Trees: " + str(depth)
-        model_unfit = DecisionTreeClassifier(max_depth=depth)
-        tree_dict[tree_param] = test_models(model_unfit, False, train_variable,\
-            train_features, test_variable, test_features)
+        model_tree = DecisionTreeClassifier(max_depth=depth)
+        models_lst.append(tuple([TREE, tree_param, model_tree]))
         for trees in NUM_TREES:
             for feat in MAX_FEATURES:
                 for leaf in MAX_LEAVES:
@@ -152,46 +121,30 @@ def forest_modeling(train_variable, train_features, test_variable,\
                         ", Max Depth of Trees: " + str(depth) + \
                         ", Max Fraction of Features: " + str(feat) +\
                         ", Max Number of Leaves: " + str(leaf)
-                    model_unfit = RandomForestClassifier(n_estimators=trees,
+                    model_forest = RandomForestClassifier(n_estimators=trees,
                         max_depth=depth, max_features=feat, max_leaf_nodes=leaf)
-                    forest_dict[param] = test_models(model_unfit, False,
-                        train_variable, train_features, test_variable,
-                        test_features)
-                    model_unfit = ExtraTreesClassifier(n_estimators=trees,
+                    model_extra = ExtraTreesClassifier(n_estimators=trees,
                         max_depth=depth, max_features=feat, max_leaf_nodes=leaf)
-                    extra_dict[param] = test_models(model_unfit, False,
-                    	train_variable, train_features, test_variable,
-                    	test_features)
-    return forest_dict, extra_dict, tree_dict
+                    model_embed = RandomTreesEmbedding(n_estimators=trees,
+                        max_depth=depth, max_features=feat, max_leaf_nodes=leaf)
+                    models_lst.append(tuple([FOREST, param, model_forest]))
+                    models_lst.append(tuple([EXTRA, param, model_extra]))
+                    models_lst.append(tuple([EMBED, param, model_extra]))
+    return models_lst
 
-def ada_boost_modeling(train_variable, train_features, test_variable,\
-    test_features):
+def ada_boost_modeling():
     '''
     Creates multiple AdaBoost models
-
-    Inputs:
-        train_variable: pandas series of variable column for training set
-        train_features: pandas dataframe of features for training set
-        test_variable: pandas series of variable column for testing set
-        test_features: pandas dataframe of features for testing set
-
-    Outputs:
-        ada_dict: a dictionary with all information about all ada boost models
     '''
-    ada_dict = {}
-    N_ESTIMATORS = [10, 30, 50, 100, 150, 200, 300]
-    LEARNING_RATE = [0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 5.0]
+    models_lst = []
     for n in N_ESTIMATORS:
         for rate in LEARNING_RATE:
             param = "Estimators: " + str(n) + ", Learning Rate: " + str(rate)
-            model_unfit = AdaBoostClassifier(n_estimators=n,\
-                learning_rate=rate)
-            ada_dict[param] = test_models(model_unfit, False, train_variable,\
-                train_features, test_variable, test_features)
-    return ada_dict
+            model = AdaBoostClassifier(n_estimators=n, learning_rate=rate)
+            models_lst.append(tuple([ADA_BOOSTING, param, model]))
+    return models_lst
 
-def bagging_modeling(train_variable, train_features, test_variable,\
-    test_features):
+def bagging_modeling():
     '''
     Creates multiple bagging models
 
@@ -204,15 +157,18 @@ def bagging_modeling(train_variable, train_features, test_variable,\
     Outputs:
         bag_dict: a dictionary with all information about all bagging models
     '''
-    bag_dict = {}
-    N_ESTIMATORS = [5, 10, 30, 50]
-    MAX_SAMPLES = [10, 50, 100, 500]
-
+    models_lst = []
     for n in N_ESTIMATORS:
         for sample in MAX_SAMPLES:
-            param = "Estimators: " + str(n) + ", Samples: " + str(sample)
-            model_unfit = BaggingClassifier(n_estimators=n,\
-                max_samples=sample)
-            bag_dict[param] = test_models(model_unfit, False, train_variable,\
-                train_features, test_variable, test_features)
-    return bag_dict
+            for feat in MAX_FEATURES:
+                param = "Estimators: " + str(n) +\
+                    ", Samples: " + str(sample) +\
+                    ", Features: " + str(feat)
+                model_bag = BaggingClassifier(n_estimators=n,
+                    max_samples=sample, max_features=feat,
+                    bootstrap_features=True)
+                model_iso = IsolationForest(n_estimators=n,
+                    max_samples=sample, max_features=feat, bootstrap=True)
+                models_lst.append(tuple([BAGGING, param, model_bag]))
+                models_lst.append(tuple([ISOLATION, param, model_iso]))
+    return models_lst
