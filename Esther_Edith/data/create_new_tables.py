accde6832e77944caf9f70bc2d46d70917b2ba7a
eespurlock <49249219+eespurlock@users.noreply.github.com>
2019-05-15 13:53:12 -0500

    Update create_new_tables.py
    writing directly to csv

--- Esther_Edith/data/create_new_tables.py
+++ Esther_Edith/data/create_new_tables.py
@@ -4,6 +4,7 @@ Bringing in new data
 
 import pandas as pd
 import numpy as np
+import csv
 
 def import_data():
     '''
@@ -12,22 +13,18 @@ def import_data():
         'Handler Sequence Number','Hazardous Waste Stream GM Form Page Number',
         'Hazardous Waste Code Owner','Hazardous Waste Code']
 
-    df_lst = []
-
     base_name = 'BR_GM_WASTE_CODE_0.txt'
-    with open(base_name) as CA:
-        i = 1
-        for line in CA:
+    with open(base_name) as orig:
+        with open(csv_filename, 'w') as to_put:
+        our_writer = csv.writer(to_put, delimiter=",")        
+        our_writer.writerow(col_list)
+        for line in orig:
             this_lst = []
             y = 0
             for x in [13,15,16,22,27,29,35]:
                 this_lst.append(line[y:x])
                 y = x
-                print(i, ": ", x)
-                i += 1
-            df_lst.append(this_lst)
-    df_down = pd.DataFrame(np.array(df_lst), columns=col_list)
-    df_down.to_csv("Biennial_Report_GM_Waste_Code.csv")
+            our_writer.writerow(this_lst)
 
     '''
     col_list = ['EPA Handler ID','Area Sequence Number',
