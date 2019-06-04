2b63ae31343549eeccf15c7e7cc24ce1761ab6d3
davefoote <dfoote@uchicago.edu>
2019-06-04 18:38:15 -0500

    changed to ordered dict for cleaners slashes

--- full_pipeline_files/cleaners.py
+++ full_pipeline_files/cleaners.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from collections import OrderedDict
 
 '''
 cleaning functions for the epa pipeline
@@ -23,8 +24,8 @@ def clean_and_converttodatetime_slashes(df, stand_form_col, start_date):
     '''
     looks for out of range dates and brings them to range
     '''
-    oor_dict = {'19': 2000, '1919': 100, '1943': 50, '1971': 20, '1974': 20,
-               '1979': 18}
+    oor_dict = OrderedDict([('19', 2000), ('1919', 100,) ('1943', 50),
+                            ('1971', 20), ('1974', 20), ('1979', 18)])
     df[['M','D','Y']] = df[stand_form_col].str.split(pat='/', expand=True)
     df['Y'] = df.Y.astype(int)
 
