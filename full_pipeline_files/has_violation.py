25c2a1a6b58c036c689ae5fc763e41ec8b52fbfa
Rachel Rose Whaley <rwhaley@uchicago.edu>
2019-06-04 14:31:05 -0500

    working initial dataframe

--- full_pipeline_files/has_violation.py
+++ full_pipeline_files/has_violation.py
@@ -3,9 +3,8 @@ Takes in violations db and returns whether or not a facility
 has had a violation within the time frame
 '''
 import pandas as pd
+import numpy as np
 
-violation_df = pd.read_csv('RCRA_VIOLATIONS.csv')
-facilities_df = pd.read_csv('RCRA_FACILITIES.csv')
 
 def has_violation(facilities_df, violations_df, start_year, end_year):
     '''
@@ -26,20 +25,59 @@ def has_violation(facilities_df, violations_df, start_year, end_year):
         years.append(y)
         y += 1
     for fid in facilities_df[fac_id]:
-        for y in year:
+        for y in years:
             id_lst.append(fid)
             year_lst.append(y)
 
     data = {fac_id: id_lst, eval_year: year_lst}
-    facs_by_year = pd.dataframe(data=data)
-    return facs_by_year
+    facs_by_year = pd.DataFrame(data=data)
+    # return facs_by_year
 
     violations_df[['M','D','Y']] = violations_df[date]\
         .str.split('/', expand=True)
-    violations_df['Y'] = violations_df['Y'].astype(int)
-    filt = (violations_df['Y'] >= start_year and\
-        violations_df['Y'] <= end_year)
-    filt_vio = violations_df[filt]
-    
+    violations_df[eval_year] = violations_df['Y'].astype(int)
 
-    
+    print(facs_by_year.head())
+
+    violation_df['HasViolation'] = 1
+    facs_by_year = pd.merge(facs_by_year, violation_df[['HasViolation',fac_id, eval_year]], left_on=[fac_id, eval_year], right_on=[fac_id, eval_year], how='left')
+    facs_by_year['HasViolation'].fillna(0, inplace=True)
+
+
+    facs_by_year.to_csv("please_work.csv")
+
+    print(facs_by_year["HasViolation"].describe())
+
+
+    return facs_by_year
+
+
+def flag_lqg(facilities_df):
+    """Adds a column to the data frame that is 1 if the facility is an LQG."""
+    # 3 is the code for LQG according to the data documentation at
+    #   https://echo.epa.gov/tools/data-downloads/rcrainfo-download-summary#download_files
+    facilities_df["IsLQG"] = pd.Series(np.where(facilities_df.FED_WASTE_GENERATOR.str.contains("3"), 1, 0), \
+                                       facilities_df.index)
+    facilities_df["IsTSDF"] = pd.Series(np.where(facilities_df.HREPORT_UNIVERSE_RECORD.str.contains("TSDF"), 1, 0), \
+                                        facilities_df.index)
+
+    return facilities_df
+
+
+if __name__ == "__main__":
+    violation_df = pd.read_csv('RCRA_VIOLATIONS.csv')
+    facilities_df = pd.read_csv('RCRA_FACILITIES.csv')
+    has_vios_df = has_violation(facilities_df, violation_df, 2011, 2013)
+
+
+    with_lqgs = flag_lqg(facilities_df)
+
+    has_vios_df = pd.merge(has_vios_df, with_lqgs[['ID_NUMBER', "IsLQG", "IsTSDF"]], on="ID_NUMBER", how="left")
+
+    print(has_vios_df["IsLQG"].describe())
+    print(has_vios_df['IsTSDF'].describe())
+
+    print(has_vios_df.head())
+
+
+
