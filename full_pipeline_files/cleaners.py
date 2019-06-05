234a88e4882348206bd4b003bb05b3a6f351cee6
davefoote <dfoote@uchicago.edu>
2019-06-04 19:29:15 -0500

    un-screwed up git merge of cleaners

--- full_pipeline_files/cleaners.py
+++ full_pipeline_files/cleaners.py
@@ -24,14 +24,11 @@ def clean_and_converttodatetime_slashes(df, stand_form_col, start_date):
     '''
     looks for out of range dates and brings them to range
     '''
-<<<<<<< HEAD
     oor_dict = {'19': 2000, '1919': 100, '1943': 50, '1971': 20, '1974': 20,
                '1979': 18}
     oor_dict = OrderedDict(oor_dict)
-=======
     oor_dict = {'19': 2000, '1919': 100, '1943': 50,\
         '1971': 20, '1974': 20, '1979': 18}
->>>>>>> ae50a2ec9538cdac6910463d09d55aca2a1a222c
     df[['M','D','Y']] = df[stand_form_col].str.split(pat='/', expand=True)
     df['Y'] = df.Y.astype(int)
 
